1ec3ede3d835f8a01c77f148154ff6232633cca7
Daniel Cisek <cisek.dan@gmail.com>
2016-03-14 20:34:00 +0000

    Added Service Provider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace cisekdan\LaravelBase;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * @var array List of commands to register
+     */
+    private $commands = [
+        'cisekdan\LaravelBase\Command\ChangeEnvironment'
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register()
+    {
+        $this->registerCommands($this->commands);
+    }
+
+    /**
+     * Registers provided commands
+     * @param $commands List of commands to register
+     */
+    private function registerCommands($commands)
+    {
+        $this->commands(commands);
+    }
+}
